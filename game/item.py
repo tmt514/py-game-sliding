c4bc1bea8aab8f72d690b59a46fdb54dff59d74a
tmt514 <math.tmt514@gmail.com>
2019-03-23 18:37:00 -0400

    item

---
+++ game/item.py
@@ -0,0 +1,32 @@
+
+class Item:
+    def __init__(self, position, stage, ui_controller):
+        self.position = position
+        self.stage = stage
+        self.ui_controller = ui_controller
+
+    def pass_through(self, player_info):
+        pass
+
+class Coin(Item):
+    def __init__(self, position, stage, ui_controller):
+        self.position = position
+        self.stage = stage
+        self.ui_controller = ui_controller
+
+        def add_coin(self, canvas):
+            pos = (1+32*self.position[1]+16, 1+32*self.position[0]+16)
+            self.ui = canvas.create_text(*pos, text='$', font=('Times', 24, 'bold'), fill='orange')
+
+        ui_controller.use_canvas(self, add_coin)
+
+    def pass_through(self, player_info):
+        """ Returns true if this object needs destroy. """
+        player_info['score'] += 5
+        player_info['needs_update'] = True
+
+        def destroy_ui(self, canvas):
+            canvas.delete(self.ui)
+
+        self.ui_controller.add_update(self, destroy_ui)
+        return True
